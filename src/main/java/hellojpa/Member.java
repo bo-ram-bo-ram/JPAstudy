7a553164d5efb8aa084dacc09ec16cd5ee604ca1
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2023-10-22 11:09:27 +0900

    [feat]기본키 매핑 실습

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,31 +6,36 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-//@Table(name="MBR")//매핑할때 이름 바꾸고싶으면
-public class Member {
+@SequenceGenerator( name = "MEMBER_SEQ_GENERATOR", sequenceName = "MEMBER_SEQ", //sequenceName : 매핑할 데이터베이스 시퀀스 이름
+                    initialValue = 1, allocationSize = 50)//50개 db에 미리 올려놓고 하나씩
+//@TableGenerator(
+//        name = "MEMBER_SEQ_GENERATOR",
+//        table = "MY_SEQUENCES",
+//        pkColumnValue = "MEMBER_SEQ")//50개 db에 미리 올려놓고 하나씩
+        public class Member {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
     @Column(name = "name",nullable = false)
     private String username;
 
-    private Integer age;
+    public Long getId() {
+        return id;
+    }
 
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-    //ordinal하면 enum이 수정됐을때 문제발생함 무조건 string쓰기!
+    public String getUsername() {
+        return username;
+    }
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-    @Lob
-    private String description;
-
-    @Transient
-    private int temp;   //db저장x, 메모리에서만 쓰겟다
-    //Getter, Setter…
     public Member() {
     }
 }
